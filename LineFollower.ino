d9ca19693844dec71bba1512ff0a9a0c00765816
kayprojekt <36220323+kayprojekt@users.noreply.github.com>
2018-04-06 13:25:35 +0530

    Add files via upload

---
+++ LineFollower.ino
@@ -0,0 +1,55 @@
+/*Author : Amber Khanna
+ * 
+ * 
+ * 
+ *Title : LineFollower
+ *-Kayproject-
+ *#1
+ */
+  // I/P and O/P
+  #define LS 2
+  #define RS 3 
+  #define LM1 4
+  #define LM2 5
+  #define RM1 6
+  #define RM2 7 
+void setup() {
+pinMode(LS, INPUT);
+pinMode(RS, INPUT);
+pinMode(LM1, OUTPUT);
+pinMode(LM2, OUTPUT);
+pinMode(RM1, OUTPUT);
+pinMode(RM2, OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(LS)&&digitalRead(RS))
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+  if(!(digitalRead(LS))&&digitalRead(RS)) //LEFT
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+if((digitalRead(LS))&&!(digitalRead(RS))) //RIGHT
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+  if(!(digitalRead(LS))&&!(digitalRead(RS))) //STOP
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+}
